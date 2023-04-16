1cba71ce7832b667fd3347f988532f53adadc977
BAAH joseph22 <soleilbusinessaction22@gmail.com>
2023-04-16 15:47:59 +0000

    termine

--- index.js
+++ index.js
@@ -18,3 +18,53 @@ for (let index=0; index < buttonsSupprimer.length; index ++) {
        grandParent.remove()
     })
 }
+
+// gérer les bouttons plus
+for (let index = 0; index < buttonsPlus.length; index ++) {
+
+    buttonsPlus[index].addEventListener("click", ()=> {
+        let parent = buttonsPlus[index].parentElement
+        let input = parent.nextElementSibling
+        let inputValue = parseInt(input.value) 
+        input.value = inputValue + 1
+        
+        sousTotal()
+        console.log(parent) 
+        console.log(input.value)
+        
+    })
+}
+
+// gérer les bouttons moins
+for (let index = 0; index < buttonsMoins.length; index ++) {
+
+    buttonsMoins[index].addEventListener("click", ()=> {
+        let parent = buttonsMoins[index].parentElement
+        let input = parent.nextElementSibling
+        let inputValue = parseInt(input.value) 
+        if (inputValue > 0) {
+            input.value = inputValue - 1
+        }
+    
+        sousTotal()
+        console.log(parent) 
+        console.log(input.value)
+        
+    })
+}
+
+function sousTotal() {
+    var allPrices = document.getElementsByClassName("prix")
+    var sousTotalSpan = document.getElementsByClassName("soustotal")
+    for (let index = 0; index < allPrices.length; index++) {
+        const quantite = allPrices[index].parentElement.nextElementSibling.nextElementSibling.value;
+        const quantiteInt = parseInt(quantite)
+        const price = parseInt(allPrices[index].innerHTML)
+        const sousTotal = quantiteInt * price
+        sousTotalSpan[index].innerHTML = sousTotal
+        console.log(sousTotal)
+        
+    }     
+}
+
+
